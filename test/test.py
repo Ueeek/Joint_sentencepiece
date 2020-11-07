99df81df07aa55c94788b4cb9f40a0c568e08eb3
Ueeek <ueki.k.ae@m.titech.ac.jp>
2020-11-07 20:44:14 +0900

    e_step

--- test/test.py
+++ test/test.py
@@ -10,7 +10,9 @@ def Train():
     arg = {
         "src_corpus":DIR+"train1K.en",
         "tgt_corpus":DIR+"train1K.de",
+        "tgt_vocab": "./test.tgt.vocab",
         "src_vocab_size":1000,
+        "dummy_vocab_size":10000,
         "seed_vocab_size":10000,
     }
 
